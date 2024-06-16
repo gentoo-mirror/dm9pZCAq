06603cc66a41715435a4aa3b55539934f3011da5
dm9pZCAq <v@0x0c.link>
2024-06-16 20:52:44 +0300

    .scripts/git-hooks/pre-commit.d: pkgcheck.sh add --net

--- .scripts/git-hooks/pre-commit.d/pkgcheck.sh
+++ .scripts/git-hooks/pre-commit.d/pkgcheck.sh
@@ -1,4 +1,9 @@
 #!/bin/sh --
 set -ue
+
 [ -n "${GIT_HOOK_SKIP_PKGCHECK:-}" ] ||
-	[ -z "$(pkgcheck scan --staged --color=true | tee /proc/self/fd/2)" ]
+	[ -z "$(
+		pkgcheck scan --staged --net \
+			${PKGCHECK_USER_AGENT:+--user-agent="${PKGCHECK_USER_AGENT}"} \
+			--color=true | tee /proc/self/fd/2
+	)" ]
